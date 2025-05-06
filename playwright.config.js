2f7c59a6a389cb1610f6b55ed47f4a0d4052a67f
dh23iterasys <dh23@iterasys.com.br>
2025-05-05 22:47:03 -0300

    Final

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     use: {
+        browserName: 'webkit', // 'firefox'ou 'chromium' ou 'webkit'
+        headless: false, // false exibe, true oculta
         screenshot: 'only-on-failure',
         video: 'retain-on-failure'
     },
